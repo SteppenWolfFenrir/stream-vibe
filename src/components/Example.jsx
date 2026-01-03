593657d2175075c0cc72ff2cfda5438f1803817a
Valentin <steppenwolffenrir@gmail.com>
2026-01-03 23:56:00 +0300

    feat: add component button

---
+++ src/components/Example.jsx
@@ -0,0 +1,19 @@
+import Button from "@/components/Button";
+
+const Example = () => {
+  return (
+    <>
+      <h1>
+        Привет!
+      </h1>
+      <Button/>
+      <Button/>
+      <Button/>
+      <h2>
+        Снова привет!
+      </h2>
+    </>
+  )
+}
+
+export default Example;
